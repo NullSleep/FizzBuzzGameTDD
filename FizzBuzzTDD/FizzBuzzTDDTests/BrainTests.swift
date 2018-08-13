f574c414949080111d164788ed5ac4fdf274bba5
Carlos Arenas <carenasdev@gmail.com>
2018-08-13 12:24:21 -0500

    Adding the BrainTest swift file and writting the first test

--- FizzBuzzTDD/FizzBuzzTDDTests/BrainTests.swift
+++ FizzBuzzTDD/FizzBuzzTDDTests/BrainTests.swift
@@ -1,5 +1,5 @@
 //
-//  FizzBuzzTDDTests.swift
+//  BrainTests.swift
 //  FizzBuzzTDDTests
 //
 //  Created by Carlos Arenas on 8/13/18.
@@ -7,15 +7,16 @@
 //
 
 import XCTest
-@testable import FizzBuzzTDD
 
-class FizzBuzzTDDTests: XCTestCase {
+class BrainTests: XCTestCase {
     
+    // Prepares the test. Runs everyime for each test.
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
+    // Clean ups afterward. Runs everyime for each test.
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
@@ -26,11 +27,11 @@ class FizzBuzzTDDTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testIsDivisibleBuThree() {
+        let brain = Brain()
+        let result = brain.isDivisibleByThree(numnber: 3)
+        // Compares the result of the test to the expected result (in this case, true). It is expected that when asked to the 'brain' to check if
+        // 3 is divisible by 3, it will responde that is true.
+        XCTAssertEqual(result, true)
     }
-    
 }
