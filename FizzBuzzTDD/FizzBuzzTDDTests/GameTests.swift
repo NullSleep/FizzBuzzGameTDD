706799b40229bafd1e87d6fbe68e4cbaecd5aa2e
Carlos Arenas <carenasdev@gmail.com>
2018-08-23 15:26:05 -0500

    Adding the Buzz, FizzBuzz and a regular numnber tests

--- FizzBuzzTDD/FizzBuzzTDDTests/GameTests.swift
+++ FizzBuzzTDD/FizzBuzzTDDTests/GameTests.swift
@@ -37,15 +37,52 @@ class GameTests: XCTestCase {
         XCTAssertTrue(game.score == 2)
     }
     
-    func testIfMoveIsRight() {
+    func testFizzRight() {
+        // The game score has to be 2 so the game does the play the score is 3 and can test Fizz
         game.score = 2
         let result = game.play(move: "Fizz")
         XCTAssertEqual(result, true)
     }
     
-    func testFizzPlay() {
+    func testFizzWrong() {
         game.score = 1
         let result = game.play(move: "Fizz")
+        XCTAssertEqual(result, false)
+    }
+    
+    func testBuzzRight() {
+        game.score = 4
+        let result = game.play(move: "Buzz")
+        XCTAssertEqual(result, true)
+    }
+    
+    func testBuzzWrong() {
+        game.score = 1
+        let result = game.play(move: "Buzz")
+        XCTAssertEqual(result, false)
+    }
+    
+    func testFizzBuzzRight() {
+        game.score = 14
+        let result = game.play(move: "FizzBuzz")
+        XCTAssertEqual(result, true)
+    }
+    
+    func testFizzBuzzWrong() {
+        game.score = 1
+        let result = game.play(move: "FizzBuzz")
+        XCTAssertEqual(result, false)
+    }
+    
+    func testNumberRight() {
+        game.score = 102
+        let result = game.play(move: "103")
+        XCTAssertEqual(result, true)
+    }
+    
+    func testNumberWrong() {
+        game.score = 1
+        let result = game.play(move: "999")
         XCTAssertEqual(result, false)
     }
 }
